be344b8d3bdc0a891b3435019829d1ae2b43c7ff
ramity <lewisclbrown@gmail.com>
2023-05-15 20:57:58 +0000

    updated backup script to have more comments, perform a data check, echo success + condition messages + configurable source dir

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-zip -9 ./backups/$(date '+%s').zip -r ./data
+# Config
+path="./data"
+
+# Check if the directory is empty
+if [ -z "$(ls -A $path)" ]; then
+    echo "No data found in $path."
+    exit 1
+fi
+
+# Zip and export archive from data to backups dir
+zip -9 ./backups/$(date '+%s').zip -r $path
+
+# Success message
+echo "Zipped contents of $path to new zip archive."
